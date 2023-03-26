af57041a5642eb6f613db13eff95e499af5b22ff
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-03-26 04:07:45 -0300

    feat: add tailwind dependency

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <section class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 container mx-auto">
+    <p v-for="item in itens" :key="item.id">{{ item.title }}</p>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      itens: [
+        { id: 1, title: "Using Tailwind CSS" },
+        { id: 2, title: "Using Tailwind CSS" },
+        { id: 3, title: "Using Tailwind CSS" },
+        { id: 4, title: "Using Tailwind CSS" },
+        { id: 5, title: "Using Tailwind CSS" },
+        { id: 6, title: "Using Tailwind CSS" },
+        { id: 7, title: "Using Tailwind CSS" },
+        { id: 8, title: "Using Tailwind CSS" },
+        { id: 9, title: "Using Tailwind CSS" }
+      ],
+    };
+  },
+  methods: {
+    getItems() {
+      return this.itens;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
