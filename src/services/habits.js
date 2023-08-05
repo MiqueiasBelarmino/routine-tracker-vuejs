7ad0264ed98d613d2bf719ae2b0b2c89b2aa9372
Miquéias Belarmino <miqueiascostabac@gmail.com>
2023-08-04 22:29:26 -0300

    refact: add user id in request

--- src/services/habits.js
+++ src/services/habits.js
@@ -1,21 +1,12 @@
 import api from "@/services/api"
 import { getStoreToken, getUser } from "@/utils/helpers";
 
-export async function getHabits() {
-    try {
-        api.defaults.headers.common['Authorization'] = `Bearer ${getStoreToken()}`;
-        const { data } = await api.get('/habits');
-        return data;
-    } catch (error) {
-        console.log(error?.message)
-    }
-}
-
 export async function getHabitsByDay(date) {
     try {
         api.defaults.headers.common['Authorization'] = `Bearer ${getStoreToken()}`;
         const { data } = await api.post(`/day`, {
-            date: date
+            date: date,
+            user_id: getUser().id
         });
         return data;
     } catch (error) {
@@ -26,7 +17,10 @@ export async function getHabitsByDay(date) {
 export async function toggleHabit(id, date) {
     try {
         api.defaults.headers.common['Authorization'] = `Bearer ${getStoreToken()}`;
-        const { data } = await api.patch(`/habits/${id}/toggle`, { date: date });
+        const { data } = await api.patch(`/habits/${id}/toggle`, {
+            date: date,
+            user_id: getUser().id
+        });
         return data;
     } catch (error) {
         console.log(error?.message)
@@ -59,7 +53,9 @@ export async function createHabit(habit) {
 export async function deleteHabit(id) {
     try {
         api.defaults.headers.common['Authorization'] = `Bearer ${getStoreToken()}`;
-        const { data } = await api.delete(`/habits/${id}`);
+        const { data } = await api.delete(`/habits/${id}`, {
+            user_id: getUser().id
+        });
         return data;
     } catch (error) {
         console.log(error?.message)
