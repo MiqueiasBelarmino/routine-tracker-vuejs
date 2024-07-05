6256d2cbb03986c02331bf68f9aea66fa2ac8a04
Miquéias Belarmino <miqueiascostabac@gmail.com>
2024-07-05 10:46:42 -0300

    refact: separete TabGroup into a component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -59,59 +59,26 @@
         </template>
       </base-modal>
 
-      <TabGroup>
-        <TabList class="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-          <base-tab title="Habits" />
-          <base-tab title="Tasks" />
-        </TabList>
-
-        <div v-if="isLoading" class="flex justify-center">
-          <div
-            class="mt-3 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-neutral-100 motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span
-              class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-              >Loading...</span
-            >
-          </div>
-        </div>
-        <TabPanels
-          v-else-if="!isLoading && (availableHabits || availableTasks)"
-          class="mt-4"
-        >
-          <base-tab-panel
-            v-if="availableHabits"
-            @click-edit="openCreateHabitModal"
-            @click-remove="openRemoveModal"
-            @toggle="toggleHabit"
-            title="Habits"
-            :items="availableHabits"
-          />
-          <base-tab-panel
-            v-if="availableTasks"
-            @click-edit="openCreateTaskModal"
-            @click-remove="openRemoveModal"
-            @toggle="toggleSelectedTask"
-            title="Tasks"
-            :items="availableTasks"
-          />
-        </TabPanels>
-      </TabGroup>
+      <Tabs
+        :availableHabits="availableHabits"  
+        :availableTasks="availableTasks"
+        :open-create-habit-modal="openCreateHabitModal"
+        :open-create-task-modal="openCreateTaskModal"
+        :open-remove-modal="openRemoveModal"
+        :toggle-habit="toggleHabit"
+        :toggle-selected-task="toggleSelectedTask"
+      />
     </div>
   </base-container>
 </template>
 
 <script>
-import { TabGroup, TabList, TabPanels } from "@headlessui/vue";
 import Header from "@/components/Header.vue";
 import BaseContainer from "@/shared/BaseContainer.vue";
 import EditIcon from "@/components/icons/EditIcon.vue";
 import RemoveIcon from "@/components/icons/RemoveIcon.vue";
 import CheckCircleIcon from "@/components/icons/CheckCircleIcon.vue";
 import BaseBox from "@/components/BaseBox.vue";
-import BaseTab from "@/components/BaseTab.vue";
-import BaseTabPanel from "@/components/BaseTabPanel.vue";
 import BaseWeekDayPicker from "@/shared/BaseWeekDayPicker.vue";
 import BaseModal from "@/shared/BaseModal.vue";
 import {
@@ -128,14 +95,12 @@ import {
   DialogPanel,
   Disclosure,
   DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
+  DisclosurePanel
 } from "@headlessui/vue";
 import { getUser } from "@/utils/helpers";
 import { createTask, getTasksByDay, toggleTask } from "@/services/tasks";
+import Tabs from "@/components/Tabs.vue";
+
 export default {
   name: "HomeView",
   components: {
@@ -144,11 +109,6 @@ export default {
     RemoveIcon,
     CheckCircleIcon,
     BaseBox,
-    TabGroup,
-    TabList,
-    TabPanels,
-    BaseTab,
-    BaseTabPanel,
     BaseModal,
     BaseWeekDayPicker,
     BaseDayPicker,
@@ -159,11 +119,8 @@ export default {
     Disclosure,
     DisclosureButton,
     DisclosurePanel,
-    Popover,
-    PopoverButton,
-    PopoverGroup,
-    PopoverPanel,
-    Header
+    Header,
+    Tabs
   },
   data() {
     return {
